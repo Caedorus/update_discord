1d2d66a6f745c868a258c53b633f097f08d22f36
Caedem <130740741+Caedorus@users.noreply.github.com>
2026-02-04 13:24:10 +0000

    Fix update_discord.sh
    Four changes:
    Reworded the Contributed section
    Reworded the password prompt message
    Added --keep-cwd because otherwise we can't find the deb
    Removed sudo -k as it is not needed with pkexec

--- update_discord.sh
+++ update_discord.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-# Made by Caedorus, forked and amended by Kharakhov.
+# Made by Caedorus
+# Contributed by Kharakhov
 # This little script compares the version of the currently installed discord to the latest one, then downloads and updates if required.
 # If discord is not installed, it will install it.
 # Compatible with all systems that install discord from a deb package, use dpkg, and pkexec. This basically means debian and derivatives.
@@ -17,9 +18,8 @@ update() {
   echo "Updating from $OLD_VERSION to $NEW_VERSION!"
   mkdir -p /tmp/update_discord && pushd /tmp/update_discord # make and go to a temporary directory. pushd ensures we can go back to wherever we used to be
   wget https://discord.com/api/download?platform=linux -O discord_latest.deb || exit 1 # download and fail if the download fails
-  echo "Your password is required and will now be prompted for to install this update. To cancel, hit cancel or press Ctrl+C."
-  pkexec dpkg -i discord_latest.deb || exit 1 # install and fail if the install fails
-  pkexec -k # remove the previous authentication record. Otherwise, a malicous discord could get root access without needing a password.
+  echo "You will now be prompted for your password to install this update. To cancel, hit cancel or press Ctrl+C."
+  pkexec --keep-cwd dpkg -i discord_latest.deb || exit 1 # install and fail if the install fails
   echo "Cleaning up..."
   popd # return to the previous directory
   rm -rf /tmp/update_discord # clean the temp directory
